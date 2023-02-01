4a3eb96b26370ac9a49b508bb0f6fafb6b69dc29
Min Park <staedi@gmail.com>
2023-02-01 15:35:33 +0900

    test

--- generic.py
+++ generic.py
@@ -35,7 +35,11 @@ def load_data(dir='data',encoding='utf-8-sig'):
         df = df[['region','symbol']]
         ref_data = pd.concat([ref_data,df])
 
+    print(raw_data.merge(ref_data,how='left',on=['region','symbol'],indicator=True))
+
     data = raw_data.merge(ref_data,how='outer',on=['region','symbol'],indicator=True)
+
+    print(data)
 
     data['valid'] = data['_merge'].apply(lambda x:'OK' if x in ('both','right_only') else 'Not Available')
 
